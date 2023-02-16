a450610160aa807d601a174f5c67c2240af9479a
Mamon Sahid <83282903+Mamon-ui@users.noreply.github.com>
2023-02-16 18:21:13 +0530

    update code

--- app.py
+++ app.py
@@ -1,49 +1,17 @@
-from flask import Flask, request
 import cv2
 import numpy as np
 import face_recognition
 import os
 from datetime import datetime
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-path = r'F:\xampp\htdocs\video\Images_Attendance'
-images = []
-classNames = []
-myList = os.listdir(path)
-for cl in myList:
-    curImg = cv2.imread(f'{path}/{cl}')
-    images.append(curImg)
-    classNames.append(os.path.splitext(cl)[0])
-
-def findEncodings(images):
-    encodeList = []
-    for img in images:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
-    return encodeList
-
-def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
-        myDataList = f.readlines()
-        nameList = []
-        for line in myDataList:
-            entry = line.split(',')
-            nameList.append(entry[0])
-        if name not in nameList:
-            time_now = datetime.now()
-            tString = time_now.strftime('%H:%M:%S')
-            dString = time_now.strftime('%d/%m/%Y')
-            f.writelines(f'\n{name},{tString},{dString}')
-
-encodeListKnown = findEncodings(images)
-
+CORS(app)
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
-        path = r'F:\xampp\htdocs\video\Images_Attendance'
+        path = '\Images_Attendance' # r'/home/arindam24/mysite/Images_Attendance'
         images = []
         classNames = []
         myList = os.listdir(path)
@@ -59,19 +27,6 @@ def predict():
                 encode = face_recognition.face_encodings(img)[0]
                 encodeList.append(encode)
             return encodeList
-
-        def markAttendance(name):
-            with open('Attendance.csv', 'r+') as f:
-                myDataList = f.readlines()
-                nameList = []
-                for line in myDataList:
-                    entry = line.split(',')
-                    nameList.append(entry[0])
-                if name not in nameList:
-                    time_now = datetime.now()
-                    tString = time_now.strftime('%H:%M:%S')
-                    dString = time_now.strftime('%d/%m/%Y')
-                    f.writelines(f'\n{name},{tString},{dString}')
 
         encodeListKnown = findEncodings(images)
 
@@ -108,10 +63,16 @@ def predict():
                     name = classNames[matchIndex].upper()
                     print("flag 3 passed")
                     video.release()
+                    response = jsonify({'id': name})
+                    response.headers.add("Access-Control-Allow-Origin", "*")
+                    response.headers.add("Access-Control-Allow-Headers", "*")
+                    response.headers.add("Access-Control-Allow-Methods", "*")
+                return response
         #return jsonify({"attendance": attendance})
-            return name
+        
+            #return name
     except Exception as e:
         return("An error occurred:", str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
